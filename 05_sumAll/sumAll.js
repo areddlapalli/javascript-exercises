0178691d5e39b7f6294ea4d6bf5f381af0ab905c
areddlapalli <areddlapalli@outlook.com>
2024-06-25 17:47:47 -0500

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+    return "ERROR";
+  }
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  let startNum = 0, endNum = 0;
+  if (num1 > num2) {
+    startNum = num2;
+    endNum = num1;
+  } else {
+    startNum = num1;
+    endNum = num2;
+  }
+
+  let result = 0;
+
+  for (let i = startNum; i <= endNum; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
