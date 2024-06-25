13928c08c8f0ddc72fcec3cc0ec5b3a4cecab99a
areddlapalli <areddlapalli@outlook.com>
2024-06-24 19:03:40 -0500

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, num) {
+  let arrRemove = [...arguments];
+  arrRemove.shift();
+  let result = [];
 
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+
+    for (let j = 0; j < arrRemove.length; j++) {
+      if (arr[i] === arrRemove[j]) {
+        result.pop();
+        break;
+      }
+    }
+  }
+
+  return result;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
